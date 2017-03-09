e13107d30cde010bedfa29e89420dbfc43d17484
alice1009tw <alice1009tw@gmail.com>
2017-03-09 15:00:57 +0800

    2017/03/09 commit and push

---
+++ src/oo/abstraction/Sneaker.java
@@ -0,0 +1,14 @@
+package oo.abstraction;
+
+public class Sneaker {
+	String brand;
+	float size;
+	String color;
+	String function;
+	public Sneaker(String brand, float size, String color, String function){
+		this.brand = brand;
+		this.size = size;
+		this.color = color;
+		this.function = function;
+	}
+}
---
+++ src/oo/abstraction/SneakerTester.java
@@ -0,0 +1,9 @@
+package oo.abstraction;
+
+public class SneakerTester {
+
+	public static void main(String[] args) {
+		Sneaker s1 = new Sneaker("Nike", (float)7.5, "Black", "Running");
+	}
+
+}
