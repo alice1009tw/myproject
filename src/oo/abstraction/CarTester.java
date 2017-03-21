28f0b3ea5f1ab0785e1799736cfb51553bc0939c
user <user@es302n22>
2017-03-21 16:23:08 +0800

    array cars[]

--- src/oo/abstraction/CarTester.java
+++ src/oo/abstraction/CarTester.java
@@ -10,6 +10,27 @@ public class CarTester {
 		
 		Car c2 = new Car("HONDA", 2000, "ACCORD", "");
 		Car c3 = new Car("TOYOTA", 2000, "WISH", "");
+		int[] n = new int[5];
+		System.out.println(n[2]);  //自動給定初始值0
+		
+		Car[] cars = new Car[3];
+		System.out.println(cars[0]);
+		cars[0] = new Car("TOYOTA", 1800, "ALTIS", "SEDAN");
+		cars[1] = new Car("HONDA", 2000, "ACCORD", "");
+		cars[2] = new Car("TOYOTA", 2000, "WISH", "");
+		cars[0].id = 301;
+		cars[1].id = 400;
+		cars[2].id = 403;
+		System.out.println(cars[0].name);
+		int i = 0;
+		for(i=0; i<3; i++){
+			System.out.println(cars[i].name);
+		}
+		System.out.println(i);  //如果沒有int i = 0; i出了迴圈就不存在了
+		
+		class AA{
+			AA a = new AA();
+		}
 	}
 
 }
