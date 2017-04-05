e26d61001ea839a54909304f9209e699e57b55c1
游雅涵 <alice1009tw@gmail.com>
2017-04-05 22:58:12 +0800

    20170405

--- src/oo/abstraction/Invoice.java
+++ src/oo/abstraction/Invoice.java
@@ -7,17 +7,18 @@ public class Invoice {
 
 	public static void main(String[] args) {
 		
-		ArrayList<String> a = new ArrayList<>();
-		a.add("331");
-		a.add("821");
-		a.add("886");
-		a.add("554");
+		ArrayList<Integer> a = new ArrayList<>();
+		a.add(331);
+		a.add(821);
+		a.add(886);
+		a.add(554);
 		System.out.println("請輸入發票號碼:");
 		Scanner scanner = new Scanner(System.in);
 		String s = scanner.nextLine();
-		s = (String)s.substring(s.length()-3);
+		s = s.substring(s.length()-3);
+		int i = Integer.parseInt(s);
 		System.out.println(s);
-		if(s==a.get(0) ||  s==a.get(1) || s==a.get(2) || s==a.get(3)){
+		if(i==a.get(0) ||  i==a.get(1) || i==a.get(2) || i==a.get(3)){
 			System.out.println("恭喜您中獎了！");
 		}else{
 			System.out.println("沒有中獎！");
