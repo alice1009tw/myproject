f2947780443ab3483dc61ac328f054985417b9d0
游雅涵 <alice1009tw@gmail.com>
2017-05-02 00:36:40 +0800

    20170501practice

--- src/oo/abstraction/Sneaker.java
+++ src/oo/abstraction/Sneaker.java
@@ -6,10 +6,28 @@ public class Sneaker {
 	String color;
 	String function;
 	int publish;
+	int amount;
+	int buyamount;
 	public Sneaker(String brand, float size, String color, String function){
 		this.brand = brand;
 		this.size = size;
 		this.color = color;
 		this.function = function;
+	}
+	public Sneaker(String brand, String color){
+		this(brand, 7.5f, color, "");
+	}
+	
+	public void abc(){
+		
+	}
+	
+	public void setAmount(int n){
+		amount = n;
+	}
+	
+	public int minusAmount(int n){
+		amount = n-buyamount;
+		return amount;
 	}
 }
