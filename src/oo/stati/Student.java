9f5585ccbf1c6e087379b147b0491956ea4c1548
alice1009tw <alice1009tw@gmail.com>
2017-05-09 14:51:06 +0800

    20170509 extends Object toString()

--- src/oo/stati/Student.java
+++ src/oo/stati/Student.java
@@ -1,33 +1,42 @@
 package oo.stati;
 
-public class Student {  //如果把public刪掉 在其他package中無法import此類別的任何東西
+public class Student { // 如果把public刪掉 在其他package中無法import此類別的任何東西
 	int english;
 	int math;
 	int chinese;
-	static int pass = 60;  //加上static pass就變成類別變數
-	public Student(){
-		
+	static int pass = 60; // 加上static pass就變成類別變數
+
+	public Student() {
+
 	}
-	static{
-		System.out.println("Static!");  //static在沒有物件的情況下也會最先執行
-//		System.out.println(english);(錯誤)  因為static在執行完後才跑出english物件
-		System.out.println(pass);  //因為pass也是static
+
+	static {
+		System.out.println("Static!"); // static在沒有物件的情況下也會最先執行
+		// System.out.println(english);(錯誤) 因為static在執行完後才跑出english物件
+		System.out.println(pass); // 因為pass也是static
 	}
-	public Student (int english, int math, int chinese){
+
+	public Student(int english, int math, int chinese) {
 		super();
 		this.english = english;
 		this.math = math;
 		this.chinese = chinese;
 	}
-	public void print(){
-		System.out.println(getScore(english)+"\t"+getScore(math)+"\t"+getScore(chinese));
+
+	public void print() {
+		System.out.println(getScore(english) + "\t" + getScore(math) + "\t" + getScore(chinese));
 	}
-	
-	protected String getScore(int score){  //protected才能讓GraduateStudent這個子類別使用
-		if(score<pass){
-			return String.valueOf(score+"*");
-		}else{
+
+	protected String getScore(int score) { // protected才能讓GraduateStudent這個子類別使用
+		if (score < pass) {
+			return String.valueOf(score + "*");
+		} else {
 			return String.valueOf(score);
 		}
 	}
+	@Override
+	public String toString() {  //java自動繼承Object類別 Object中有toString方法
+		return "english:" + english + "\n" + "chinese:" + chinese + "\n" + "math:" + math;
+	}
+
 }
--- src/oo/test/Tester.java
+++ src/oo/test/Tester.java
@@ -5,30 +5,31 @@ import java.util.ArrayList;
 import oo.abstraction.*;
 import oo.stati.Student;
 
-public class Tester {
+public class Tester { // java中會自動繼承Object類別
 
 	public static void main(String[] args) {
+		Student stu = new Student();
+		System.out.println(stu);
+
 		Car c = new Car("TOYOTA", 1800, "ALTIS", "SEDAN");
-		ArrayList<String> a = new ArrayList();  //在<>裡定義資料型態
-//		String[] numbers = {"331", "821", "886", "554"};
+		ArrayList<String> a = new ArrayList(); // 在<>裡定義資料型態
+		// String[] numbers = {"331", "821", "886", "554"};
 		a.add("331");
 		a.add("821");
-//		a.add(123);
+		// a.add(123);
 		a.add("886");
-//		a.add(true);
+		// a.add(true);
 		System.out.println(a.size());
 		a.add("554");
 		System.out.println(a.size());
 		a.set(2, "885");
 		System.out.println(a);
-		String data = (String)a.get(0);
+		String data = (String) a.get(0);
 		System.out.println(data);
 		System.out.println(a.get(1));
-//		int n = (int)a.get(4);
-		Student stu = new Student();
-//		如果Student類別中的Student()前有加public才能在這個package中使用
-		
-	}
-	
+		// int n = (int)a.get(4);
+		// Student stu = new Student();
+		// 如果Student類別中的Student()前有加public才能在這個package中使用
 
+	}
 }
