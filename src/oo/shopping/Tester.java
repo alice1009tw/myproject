023d77a7b2751ce4fd2ff0b0aaff957761723560
alice1009tw <alice1009tw@gmail.com>
2017-05-16 00:52:45 +0800

    class Tester

--- src/oo/shopping/Tester.java
+++ src/oo/shopping/Tester.java
@@ -6,12 +6,13 @@ public class Tester {
 		Customer c = new Customer();
 		SilverCustomer s = new SilverCustomer();
 		GoldenCustomer g = new GoldenCustomer();
-		System.out.println("一般會員:"+c.money(6000));
-		System.out.println("銀級會員:"+c.money(8000)+"\t"+s.discount(8000));
-		System.out.println("銀級會員:"+c.money(10000)+"\t"+s.discount(10000));
-		System.out.println("金級會員:"+c.money(30000)+"\t"+s.discount(30000)+"\t"+g.bonus(30000));
-		System.out.println("一般會員:"+c.money(2000));
-		System.out.println("金級會員:"+c.money(10000)+"\t"+s.discount(10000)+"\t"+g.bonus(10000));
+		System.out.println("金額"+"\t"+"折扣後"+"\t"+"回饋金");
+		System.out.println(c.money(6000));
+		System.out.println(c.money(8000)+"\t"+s.discount(8000));
+		System.out.println(c.money(10000)+"\t"+s.discount(10000));
+		System.out.println(c.money(30000)+"\t"+s.discount(30000)+"\t"+g.bonus(30000));
+		System.out.println(c.money(2000));
+		System.out.println(c.money(10000)+"\t"+s.discount(10000)+"\t"+g.bonus(10000));
 	}
 
 }
