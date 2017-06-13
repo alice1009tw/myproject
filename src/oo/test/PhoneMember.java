b878e0c9055ba5384a044edd9c6e51eded59cb7e
alice1009tw <alice1009tw@gmail.com>
2017-06-13 15:54:10 +0800

    getter setter

--- src/oo/test/PhoneMember.java
+++ src/oo/test/PhoneMember.java
@@ -3,4 +3,16 @@ package oo.test;
 public class PhoneMember extends Member{
 	String phone;
 	boolean verified = false;
+	public String getPhone(){
+		return phone;
+	}
+	public void setPhone(){
+		this.phone = phone;
+	}
+	public boolean isVerified(){  //boolean的getter是用is命名
+		return verified;
+	}
+	public void setVerified(){
+		this.verified = verified;
+	}
 }
